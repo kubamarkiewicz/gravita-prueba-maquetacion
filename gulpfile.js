bec50a290031a04ff004c40ecd2b115193ef8da2
Kuba Markiewicz <kuba.markiewicz@gmail.com>
2018-11-05 00:09:12 +0100

    init repo

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('styles', function() {
+    gulp.src('sass/**/*.scss')
+    	.pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./css/'))
+});
+
+//Watch task
+gulp.task('default',function() {
+    gulp.watch('sass/**/*.scss',['styles']);
+});
